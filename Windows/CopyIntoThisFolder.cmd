bec5fbf0dbedbafae1e012cfcebbe0fdaf5fdf7c
Cvndas <eidasmedia@outlook.com>
2025-09-02 08:47:56 +0200

    Added keybinds and settings.json

--- Windows/CopyIntoThisFolder.cmd
+++ Windows/CopyIntoThisFolder.cmd
@@ -2,6 +2,12 @@
 set "CustomSakuraThemePath=C:\Users\Thad9\.vscode\extensions\v37r1x07.sakura-0.0.7\themes\Sakura-color-theme.json"
 set "CustomSakuraThemeError=Failed to copy Custom Sakura Theme"
 
+set "KeybindsPath=C:\Users\Thad9\AppData\Roaming\Code\User\keybindings.json"
+set "KeybindsError=Failed to copy the keybindings.json"
+
+set "userSettingsPath=C:\Users\Thad9\AppData\Roaming\Code\User\settings.json"
+set "userSettingsError=Failed to copy settings.json"
+
 
 ::: The copying
 if exist "%CustomSakuraThemeError%" del "%CustomSakuraThemeError%"
@@ -11,4 +17,15 @@ if errorLevel 1 (
 )
 
 
+if exist "%KeybindsError%" del "%KeybindsError%"
+copy "%KeybindsPath%" . 
+if errorLevel 1 (
+    echo "_Check filename for error message_" > "%KeybindsError%"
+)
+
+if exist "%userSettingsError%" del "%userSettingsError%"
+copy "%userSettingsPath%" . 
+if errorLevel 1 (
+    echo "_Check filename for error message_" > "%userSettingsError%"
+)
 
